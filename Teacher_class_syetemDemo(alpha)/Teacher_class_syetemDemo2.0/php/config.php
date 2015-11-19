558d323552a04da0c5a5aa41071eb11ba195e3a5
hongweipeng <961365124@qq.com>
2015-11-19 09:22:18 +0800

    删除多余文件

--- Teacher_class_syetemDemo(alpha)/Teacher_class_syetemDemo2.0/php/config.php
+++
@@ -1,7 +0,0 @@
-<?
-return array(
-    'DB_HOST'                => 'localhost', // 服务器地址
-    'DB_NAME'                => 'teacher_class_system', // 数据库名
-    'DB_USER'                => 'root', // 用户名
-    'DB_PWD'                 => '', // 密码
-    );
